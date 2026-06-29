4b045c3abea6fede2a3ac597d0cff59297d0b274
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2026-06-29 14:50:17 -0400

    Change html to dynamic js rendering

---
+++ src/home.js
@@ -0,0 +1,17 @@
+export default function () {
+  const homeHeader = document.createElement("h1");
+  homeHeader.textContent = "The Duble Taunt";
+
+  const homeText = document.createElement("p");
+  homeText.textContent =
+    "This is the best restaurant in the whole world it makes the best mac and cheese you'll ever have. Come try it, you'll never be able to have a different mac and cheese again.";
+
+  const homeImage = document.createElement("img");
+  homeImage.src =
+    "https://www.allrecipes.com/thmb/e8uotDI18ieXNBY0KpmtGKbxMRM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/238691-Simple-Macaroni-And-Cheese-mfs_008-4x3-6ed91ba87a1344558aacc0f9ef0f4b41.jpg";
+  homeImage.style.width = "300px";
+  homeImage.style.height = "300px";
+
+  const homeDiv = document.querySelector("#content");
+  homeDiv.append(homeHeader, homeText, homeImage);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("this is working!!!");
+import initialPageLoad from "./home.js";
+
+initialPageLoad();
